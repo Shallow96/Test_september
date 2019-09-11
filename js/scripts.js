b9e2c2440768a02b970a7076c14e67e373b06c17
Shallow96 <oleg_onoprienko@rambler.ru>
2019-09-12 00:42:29 +0300

    fix my bugs

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,4 @@
 $(document).ready(function () {
-    $('.owl-carousel').owlCarousel({
-        dots: false,
-        center: false,
-        loop: true,
-        margin: 0,
-        nav: false,
-        autoWidth: true,
-        padding: 0,
-        smartSpeed: 800,
-        navText:false,
-        responsive:{
-
-            0:{
-                items: 6,
-            },
-            1200:{
-                items: 6,
-            }
-        }
-    });
-});
-
-
-$(document).ready(function () {
     $(".next_quest").click( function () {
         $(".first_check").animate({left: -900},1700);
         $(".second_check").animate({left: 0},1700);
